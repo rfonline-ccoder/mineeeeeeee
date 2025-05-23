c8852d6b5099de38fc1504733557e2b7fec93481
rfonline-ccoder <rfrpofficial@gmail.com>
2025-05-23 16:14:56 +0200

    Initial commit

---
+++ start.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+title Minecraft Server - By mineleak.pro
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                          SECCIALY FOR SITE MINELEAK.PRO
+echo                        COPYRIGHT 2020-2025 ALL RIGHTS RESERVED
+echo.
+echo                           __________________________
+echo.
+echo.
+echo                                  EasyLand SETUP
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo ___________________________________________________________________________
+java -Xms1024M -Xmx4096M -jar server.jar
+pause
